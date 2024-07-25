8bc891029db6ce4b708e565ae9b52fd59a495df7
zmy <zmy20220416@gmail.com>
2024-07-25 17:35:34 +0800

    feat: first commit

---
+++ .stylelintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['stylelint-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-recess-order',
+  ],
+};
